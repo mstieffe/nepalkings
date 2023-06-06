959dd365f2eec0c144d9a621e20e2db53267f3d8
Marc Stieffenhofer <marc.stieffenhofer@zollsoft.de>
2023-06-06 17:24:44 +0200

    starting here

---
+++ settings.py
@@ -0,0 +1,21 @@
+# settings.py
+
+# Screen settings
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_CAPTION = 'Nepal Kings'
+
+# Font settings
+FONT_PATH = None
+FONT_SIZE = int(0.05 * SCREEN_HEIGHT)  # Adjust the font size based on screen height
+
+# Sizes
+SMALL_FIELD_WIDTH = int(0.375 * SCREEN_WIDTH)
+SMALL_FIELD_HEIGHT = int(0.05 * SCREEN_HEIGHT)
+
+# Color settings
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Server settings
+SERVER_URL = 'http://localhost:5000'
