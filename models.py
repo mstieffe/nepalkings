d395ab47e4a7fe143f6095ca6e741339d9f1c6ac
mstieffe <mstieffe@students.uni-mainz.de>
2023-06-12 10:57:09 +0200

    seperated model definition from server.py

--- models.py
+++ models.py
@@ -1,30 +1,56 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy_utils import ChoiceType
+import enum
 
 db = SQLAlchemy()
+
+class Suit(enum.Enum):
+    HEARTS = "Hearts"
+    DIAMONDS = "Diamonds"
+    CLUBS = "Clubs"
+    SPADES = "Spades"
+
+class Rank(enum.Enum):
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    # ... and so on
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     players = db.relationship('Player', backref='game', lazy=True)
-    cards = db.relationship('Card', backref='game', lazy=True)
+    state = db.Column(db.String(20), nullable=False, default='waiting')
 
 class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
     hand = db.relationship('Card', backref='player', lazy=True)
-    # other player-related data...
+    status = db.Column(db.String(20), nullable=False, default='active')
 
 class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
     player_id = db.Column(db.Integer, db.ForeignKey('player.id'))
-    suit = db.Column(db.String(20))
-    rank = db.Column(db.String(20))
-    # other card-related data...
+    suit = db.Column(ChoiceType(Suit, impl=db.String()))
+    rank = db.Column(ChoiceType(Rank, impl=db.String()))
 
 class Deck:
     def __init__(self, game):
         self.game = game
-        self.cards = Card.query.filter_by(game_id=game.id, player_id=None)
+        self.cards = Card.query.filter_by(player_id=None)
 
     def shuffle(self):
         random.shuffle(self.cards)
