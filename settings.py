8f3305daacad9901cbef6b20cdf3c2fe3a8402b5
mstieffe <mstieffe@students.uni-mainz.de>
2023-06-13 15:53:26 +0200

    open challenges

--- settings.py
+++ settings.py
@@ -51,7 +51,7 @@ RED = (255, 0, 0)
 # Server settings
 SERVER_URL = 'http://localhost:5000'
 
-DB_URL = 'sqlite:////tmp/test.db'
+DB_URL = 'sqlite:///test.db'
 
 def get_x(relative_position):
     return SCREEN_WIDTH*relative_position
