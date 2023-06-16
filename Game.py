1b1d99a1d93396df6b92bd2ced7bd5ead5b6cc9c
mstieffe <mstieffe@students.uni-mainz.de>
2023-06-16 21:04:46 +0200

    Game State serialize

--- Game.py
+++ Game.py
@@ -1,16 +1,83 @@
-import pygame
+import requests
 import settings
 
 class Game:
-    def __init__(self):
-        self.opponent = None
-        self.date = None
-        self.game_id = None
+    def __init__(self, game_dict, user_dict):
+        self.game_id = game_dict['id']
+        self.state = game_dict['state']
+        self.date = game_dict['date']
+        self.players = []
+        self.cards = []
+        self.opponent_name = None
+        self.player_id = None
 
-        self.board = None
-        self.turn = None
+        for player_dict in game_dict.get('players', []):
+            self.players.append({
+                'player_id': player_dict['id'],
+                'user_id': player_dict['user_id'],
+                'username': player_dict['username']
+            })
 
+        for card_dict in game_dict.get('cards', []):
+            self.cards.append({
+                'card_id': card_dict['id'],
+                'suit': card_dict['suit'],
+                'rank': card_dict['rank'],
+                'player_id': card_dict['player_id']
+            })
 
+        user_id = user_dict.get('id')
+        for player_dict in self.players:
+            if player_dict['user_id'] == user_id:
+                self.player_id = player_dict['player_id']
+            else:
+                self.opponent_name = player_dict['username']
 
-        #self.action = None
-        #self.user_response = None
+    def update(self):
+        try:
+            response = requests.get(f'{settings.SERVER_URL}/games/get_game', params={'game_id': self.game_id})
+            if response.status_code != 200:
+                #print(response.json()['message'])
+                print("Failed to update game")
+                return
+
+            game_data = response.json()
+            game = game_data.get('game')
+
+            if not game:
+                print("Game data not found in response")
+                return
+
+            self.game_id = game['id']
+            self.state = game['state']
+            self.date = game['date']
+            self.players = []
+            self.cards = []
+
+            for player_dict in game.get('players', []):
+                self.players.append({
+                    'player_id': player_dict['id'],
+                    'user_id': player_dict['user_id'],
+                    'username': player_dict['username']
+                })
+
+            for card_dict in game.get('cards', []):
+                self.cards.append({
+                    'card_id': card_dict['id'],
+                    'suit': card_dict['suit'],
+                    'rank': card_dict['rank'],
+                    'player_id': card_dict['player_id']
+                })
+
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+    def get_hand(self, is_opponent=False):
+        player_id = self.player_id if not is_opponent else next((player['player_id'] for player in self.players if player['username'] == self.opponent_name), None)
+
+        if player_id is None:
+            print("Player ID not found")
+            return []
+
+        hand = [card for card in self.cards if card['player_id'] == player_id]
+        return hand
--- GameScreen.py
+++ GameScreen.py
@@ -9,11 +9,21 @@ class GameScreen(Screen):
     def __init__(self, state):
         super().__init__(state)
 
+    def update_hand(self):
+        self.state.game.update_hand()
+
+    def update_game(self):
+        self.state.game.update()
+
     def render(self):
 
         self.window.fill(settings.BACKGROUND_COLOR)
 
-        self.draw_text(self.state.game.opponent, settings.BLACK, settings.get_x(0.1), settings.get_x(0.1))
+        self.draw_text(self.state.game.opponent_name, settings.BLACK, settings.get_x(0.1), settings.get_x(0.1))
+
+        self.draw_text(str(len(self.state.game.get_hand())), settings.BLACK, settings.get_x(0.5), settings.get_x(0.1))
+        for i, card in enumerate(self.state.game.get_hand()):
+            self.draw_text(card['suit']+" "+card['rank'], settings.BLACK, settings.get_x(0.1), settings.get_x(0.1)+i*settings.SMALL_SPACER_X)
 
         super().render()
 
@@ -22,8 +32,10 @@ class GameScreen(Screen):
     def update(self, events):
         super().update()
 
+
         current_time = pygame.time.get_ticks()
-        #if current_time - self.last_update_time >= self.update_interval:
+        if current_time - self.last_update_time >= self.update_interval:
+            self.update_game()
 
 
         #for button in self.challenge_buttons + self.open_challenge_buttons:
