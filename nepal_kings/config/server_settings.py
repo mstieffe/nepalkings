c9779be419e821b974c087590b902c80bf7f2a0d
Marc Stieffenhofer <marcstieffenhofer@marcs-mbp.speedport.ip>
2026-03-22 09:08:04 +0100

    Default SERVER_URL to remote PythonAnywhere host

--- nepal_kings/config/server_settings.py
+++ nepal_kings/config/server_settings.py
@@ -2,7 +2,7 @@
 import os
 
 # Server and database configurations
-SERVER_URL = os.getenv('SERVER_URL', 'http://localhost:5000')
+SERVER_URL = os.getenv('SERVER_URL', 'https://nepalkings.pythonanywhere.com')
 DB_URL = os.getenv('DB_URL', 'sqlite:///test.db')
 
 # Debug logging
